20dd21753298684efc5f8e30abfbee81e48395b3
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-03 03:37:02 +0000

    Style hero heading

--- resources/js/components/home/Hero.tsx
+++ resources/js/components/home/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <section className="flex flex-wrap items-center justify-center gap-10">
             <div className="grid grid-rows-3 max-w-md items-center justify-items-center lg:justify-items-start">
-                <h1>Nourishing Lives, One Meal at a Time</h1>
+                <h1 className="text-4xl font-bold text-center md:text-start">Nourishing Lives, One Meal at a Time</h1>
                 <p>MerryMeal delivers nutritious meals to elderly and vulnerable adults—because everyone deserves a hot meal and a helping hand.</p>
                 <div className="grid grid-cols-2 gap-5 items-center">
                     <Button asChild>
