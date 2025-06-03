fc40f391d168b5a0d831ed71586955fcfd5e9cde
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-03 02:45:00 +0000

    Display first name instead of full name initials

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -10,11 +10,11 @@ export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: 
             <Avatar className="h-8 w-8 overflow-hidden rounded-full">
                 <AvatarImage src={user.avatar} alt={user.name} />
                 <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                    {getInitials(user.name)}
+                    {(user.first_name)}
                 </AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate font-medium">{user.first_name}</span>
                 {showEmail && <span className="text-muted-foreground truncate text-xs">{user.email}</span>}
             </div>
         </>
