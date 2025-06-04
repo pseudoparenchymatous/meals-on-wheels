5aa623cc5f094148cda1990957152051a3ef9751
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-04 08:49:20 +0000

    Remove brand name from header

--- resources/js/components/Header.tsx
+++ resources/js/components/Header.tsx
@@ -20,9 +20,8 @@ import {
 export default () => {
     return (
         <header className="flex justify-around items-center p-3 sticky top-0 bg-background">
-            <a href={route('home')} className="flex items-center gap-2">
                 <img src="storage/images/meals-on-wheels-logo.svg" className="max-h-8" />
-                <span>Meals on Wheels</span>
+            <a href={route('home')} >
             </a>
             <Navbar />
             <div className="hidden md:inline">
