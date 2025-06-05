bf80457f071c51699d159fba4b8f5fb53f8d421f
D34TH-SE <eduardosolano09600@gmail.com>
2025-06-05 12:30:37 +0800

    Moving the contact page into Component folder

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -3,12 +3,12 @@ import { Head } from '@inertiajs/react';
 import Hero from '@/components/home/Hero';
 import ServiceFeatures from '@/components/home/ServiceFeatures';
 
-export default function Home({ user }) {
+export default function Home() {
     return (
         <Layout>
             <Head title="Welcome" />
             <Hero />
             <ServiceFeatures />
         </Layout>
-    )
+    );
 }
