4ee319eca4ffca5eb52c9385f76fab43fc0272e0
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-05 04:39:30 +0000

    Fix getting user initials

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -158,7 +158,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                                     <Avatar className="size-8 overflow-hidden rounded-full">
                                         <AvatarImage src={auth.user.avatar} alt={auth.user.name} />
                                         <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                            {getInitials(auth.user.name)}
+                                            {getInitials(auth.user.first_name, auth.user.last_name)}
                                         </AvatarFallback>
                                     </Avatar>
                                 </Button>
