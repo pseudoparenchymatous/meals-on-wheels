3edfa27fb304587815b587c3699fe40c4aed236c
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-05 10:51:23 +0000

    Update `Navbar` links

--- resources/js/components/Navbar.tsx
+++ resources/js/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@inertiajs/react';
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -5,25 +6,23 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 
+const links = [ 'home', 'about', 'contact', ];
+
+const navLinks = links.map((link, index) => {
+    return (
+        <NavigationMenuItem key={index}>
+            <NavigationMenuLink asChild>
+                <Link href={route(link)} prefetch>{link.charAt(0).toUpperCase() + link.slice(1)}</Link>
+            </NavigationMenuLink>
+        </NavigationMenuItem>
+    );
+});
+
 export default () => {
     return (
         <NavigationMenu className="hidden w-full sm:flex">
             <NavigationMenuList className="flex gap-3">
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
-                        <a href={route('home')}>Home</a>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
-                        <a href={route('about')}>About</a>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
-                        <a href={route('contact')}>Contact</a>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
+                {navLinks}
             </NavigationMenuList>
         </NavigationMenu>
     );
