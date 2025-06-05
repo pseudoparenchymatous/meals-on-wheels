0940e1d86de96dc134a5d3e2257f84389f7e50d4
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-05 04:43:25 +0000

    Remove search button

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -126,9 +126,6 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
 
                     <div className="ml-auto flex items-center space-x-2">
                         <div className="relative flex items-center space-x-1">
-                            <Button variant="ghost" size="icon" className="group h-9 w-9 cursor-pointer">
-                                <Search className="!size-5 opacity-80 group-hover:opacity-100" />
-                            </Button>
                             <div className="hidden lg:flex">
                                 {rightNavItems.map((item) => (
                                     <TooltipProvider key={item.title} delayDuration={0}>
