b290b112b13f09493fa075b97bb956e47d616cff
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-14 14:42:14 +0000

    chore: format file

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Caregiver;
 use App\Models\Member;
-use App\Models\User;
 use App\Models\Partner;
+use App\Models\User;
 use App\Models\Volunteer;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $userRules = [
-            'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'phone' => 'required|string|max:20',
             'address' => 'required|string|max:255',
@@ -57,7 +57,7 @@ class RegisteredUserController extends Controller
 
             $user = $createdUserType->user()->create([
                 'first_name' => $request->first_name,
-                'last_name'=> $request->last_name,
+                'last_name' => $request->last_name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
                 'phone' => $request->phone,
@@ -75,7 +75,7 @@ class RegisteredUserController extends Controller
     public function validateByUserType(Request $request)
     {
         if ($request->user_type == 'caregiver') {
-            Validator::make($request->all(),[
+            Validator::make($request->all(), [
                 'member.first_name' => 'required|string|max:255|exists:users,first_name',
                 'member.last_name' => 'required|string|max:255|exists:users,last_name',
             ], [
@@ -84,7 +84,7 @@ class RegisteredUserController extends Controller
             ])->validate();
         }
 
-        $rules = match($request->user_type) {
+        $rules = match ($request->user_type) {
             'member' => [
                 'birth_date' => 'required|date|before:today',
                 'diet' => 'nullable|in:vegetarian,vegan,halal,lactose_intolerant,diabetic',
@@ -104,14 +104,14 @@ class RegisteredUserController extends Controller
 
     public function createUserType(Request $request)
     {
-        return match($request->user_type) {
+        return match ($request->user_type) {
             'member' => Member::create([
-                'birth_date'=> $request->birth_date,
+                'birth_date' => $request->birth_date,
                 'diet' => $request->diet,
             ]),
             'caregiver' => Caregiver::create([
-                'member_id' => User::firstWhere('first_name', $request->member["first_name"])
-                    ->firstWhere('last_name', $request->member["last_name"])
+                'member_id' => User::firstWhere('first_name', $request->member['first_name'])
+                    ->firstWhere('last_name', $request->member['last_name'])
                     ->userable
                     ->id,
             ]),
