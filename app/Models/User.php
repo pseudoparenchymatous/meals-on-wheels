9df4b45c2946fa1f94024df80160490bb7eff5d6
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-14 14:42:28 +0000

    chore: remove unused imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
