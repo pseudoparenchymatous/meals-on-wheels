02e05ef3d98b3f3d40c50c0abb67fc51e341c0bf
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-16 14:19:29 +0800

    feat: add admin user in DatabaseSeeder

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Admin extends Model
 {
+    use HasFactory;
+
     public function user(): MorphOne
     {
         return $this->morphOne(User::class, 'userable');
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->for(
+            Admin::factory(), 'userable'
+        )->create();
+    }
+}
