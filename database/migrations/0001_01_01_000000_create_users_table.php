1a37e2077db384047dd28c50bbec6680818a7bbb
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-16 11:30:37 +0800

    Make phone number field required in users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('phone')->nullable();
+            $table->string('phone');
             $table->text('address');
             $table->morphs('userable');
             $table->timestamp('email_verified_at')->nullable();
