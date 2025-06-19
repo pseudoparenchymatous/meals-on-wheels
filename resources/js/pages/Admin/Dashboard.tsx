dc98bab2889cd5ad9bc4e265bb01a675507958c8
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-19 11:36:04 +0000

    Add `head` title to admin dashboard

--- resources/js/pages/Admin/Dashboard.tsx
+++ resources/js/pages/Admin/Dashboard.tsx
@@ -1,8 +1,10 @@
 import AdminLayout from '@/layouts/AdminLayout';
+import { Head } from '@inertiajs/react';
 
 export default function Dashboard() {
     return (
         <AdminLayout>
+            <Head title="Dashboard" />
             <div>
             </div>
         </AdminLayout>
