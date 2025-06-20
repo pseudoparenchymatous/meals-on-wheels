055806f5411c57ba022361bd289b18ae4a71ce85
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-20 02:20:46 +0000

    Add factory for `KitchenPartner` model

--- app/Models/KitchenPartner.php
+++ app/Models/KitchenPartner.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class KitchenPartner extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'org_name',
     ];
