b1cc312726d4b24f14a1c5cc41e54d59a92c97b1
Flow258 <asalquizalas@gmail.com>
2025-06-20 09:00:47 +0800

    refactor(donation): reorganize import statements for Donation components

--- resources/js/pages/Donation.tsx
+++ resources/js/pages/Donation.tsx
@@ -1,9 +1,9 @@
 import Layout from '@/layouts/Layout';
 import { Head } from '@inertiajs/react';
-import DonationHero from '@/components/home/DonationHero';
 import Hero from '@/components/home/Hero';
-import DonationForm from '@/components/home/DonationForm';
-import DonationFormDummy from '@/components/home/DonationFormDummy';
+import DonationForm from '@/components/donation/DonationForm';
+import DonationFormDummy from '@/components/donation/DonationFormDummy';
+import DonationHero from '@/components/donation/DonationHero';
 
 export default function App({ }) { // 'user' prop might come from Laravel/Inertia
   return (
