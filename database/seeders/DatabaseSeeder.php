f7cc58a6d37cef526602a67893c8b6bf9d02b539
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-20 02:32:44 +0000

    Add factory for `Rider` model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(
+        $this->call([
+            AdminSeeder::class,
             KitchenPartnerSeeder::class,
-        );
+            RiderSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/RiderSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Rider;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RiderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->for(
+            Rider::factory(), 'userable'
+        )->create();
+    }
+}
