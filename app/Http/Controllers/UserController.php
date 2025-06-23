ec3fa7bc80d15f7639a53463a40f285622e357ad
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-23 06:31:23 +0000

    admin: allow editing of users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class UserController extends Controller
@@ -13,5 +14,20 @@ class UserController extends Controller
             'users' => User::with('userable')->get(),
         ]);
     }
-}
 
+    public function edit(User $user)
+    {
+        return Inertia::render('Admin/EditUser', [
+            'user' => $user->load('userable'),
+        ]);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->userable->first_name = $request->first_name;
+        $user->userable->last_name = $request->last_name;
+        $user->userable->save();
+
+        return back()->with('message', 'User has been updated');
+    }
+}
---
+++ resources/js/pages/Admin/EditUser.tsx
@@ -0,0 +1,77 @@
+import AdminLayout from '@/layouts/AdminLayout';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Head, Link, useForm } from '@inertiajs/react';
+import { toast } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
+
+export default function EditUser({ user }) {
+    const { data, setData, patch, processing, errors, reset } = useForm({
+        first_name: user.userable.first_name,
+        last_name: user.userable.last_name,
+        org_name: user.userable.org_name
+    });
+
+    function submit(e) {
+        e.preventDefault();
+        patch(route('admin.users.update', user.id), {
+            onSuccess: () => toast.success("User has been updated"),
+        });
+    }
+
+    return (
+        <AdminLayout>
+            <Head title="Edit User" />
+            <div className="mb-2">Edit User</div>
+            <form className="border p-4 rounded-xl w-fit" onSubmit={submit}>
+                <Toaster position="top-center" richColors/>
+                {user.userable_type !== 'kitchen partner' ?
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="first_name">First Name</Label>
+                            <Input
+                                id="first_name"
+                                onChange={e => setData('first_name', e.target.value)}
+                                type="text"
+                                value={data.first_name}
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="last_name">Last Name</Label>
+                            <Input
+                                id="last_name"
+                                onChange={e => setData('last_name', e.target.value)}
+                                type="text"
+                                value={data.last_name}
+                            />
+                        </div>
+                    </div>
+                    :
+                    <div>
+                        <Label>Organization Name</Label>
+                        <Input
+                            id="org_name"
+                            onChange={e => setData('org_name', e.target.value)}
+                            type="text"
+                            value={data.org_name}
+                        />
+                    </div>
+                }
+                <div className="flex gap-3 justify-end mt-4">
+                    <Button asChild variant="outline">
+                        <Link href={route('admin.users.index')}>
+                            Cancel
+                        </Link>
+                    </Button>
+                    <Button
+                        disabled={processing}
+                        type="submit"
+                    >
+                        Update
+                    </Button>
+                </div>
+            </form>
+        </AdminLayout>
+    );
+}
