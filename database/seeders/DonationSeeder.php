4e35affd59dda492ecb0eb2398d8336e056ec891
Flow258 <asalquizalas@gmail.com>
2025-06-23 16:56:38 +0800

    feat: add DonationSeeder to populate donation data in the database

---
+++ database/seeders/DonationSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Donation;
+
+class DonationSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Donation::factory()->count(15)->create();
+    }
+}
