3329fa53908c8b457ebddaf58a66a5f4d3125f81
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-28 14:12:33 +0000

    refactor(member/verify): simplify check if variable is true

--- resources/js/pages/Member/Verify.tsx
+++ resources/js/pages/Member/Verify.tsx
@@ -11,7 +11,7 @@ export default function Verify({ verified }) {
         >
             <Head title="Account Verification" />
             <div className="grid gap-2">
-                {verified == true &&
+                {verified &&
                     <Button asChild>
                         <Link href={route('dashboard')}>Dashboard</Link>
                     </Button>
