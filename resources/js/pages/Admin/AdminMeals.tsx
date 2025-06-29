8c90fc666f043b0b7d4c1b2e193bf446d78292f8
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-06-29 10:20:55 +0000

    refactor(admin/`AdminMeals`): remove unused variables and imports

--- resources/js/pages/Admin/AdminMeals.tsx
+++ resources/js/pages/Admin/AdminMeals.tsx
@@ -1,37 +1,13 @@
 import AdminLayout from '@/layouts/AdminLayout';
-import MealForm from "@/components/MealForm";
 import Meallist from "@/components/Meallist";
 import { Head } from "@inertiajs/react";
-import { usePage } from '@inertiajs/react';
-import { NavItem } from '@/types';
-import { LayoutGrid, Apple } from 'lucide-react';
-import { useState } from 'react';
-import IngredientsTable from '@/components/IngredientsTable';
-
-const navItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/admin/dashboard',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'Meals',
-        href: '/admin/Meals',
-        icon: Apple,
-    },
-];
 
 export default function MealAdminDashboard({meals, ingredients}) {
-
     return (
         <AdminLayout>
             <Head title="Meals" />
-            <div>
-                <div className="fg-foreground text-2xl p-10 m-1">
-                    <h1 className="text-3xl font-bold pb-10">Meals Dashboard Control</h1>
-                </div>
-                <Meallist meals={meals} ingredients={ingredients}/>
-            </div>
+            <h1 className="text-3xl font-bold pb-10">Meals Dashboard Control</h1>
+            <Meallist meals={meals} ingredients={ingredients}/>
         </AdminLayout>
     );
 }
