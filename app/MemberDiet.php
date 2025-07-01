616bba6df83b704ce085aaf4e0a0b1a5f6e81acd
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-07-01 02:00:09 +0000

    Remove whitespace

--- app/MemberDiet.php
+++ app/MemberDiet.php
@@ -10,7 +10,6 @@ enum MemberDiet: string
     case LactoseIntolerant = 'Lactose Intolerant';
     case Diabetic = 'Diabetic';
 
-
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
