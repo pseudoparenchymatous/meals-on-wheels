9c5297334e217c6cb2da4bd190b5ba65e631bfa0
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-07-04 06:09:54 +0000

    fix(admin/`Plan`): add types and comments

--- resources/js/pages/Admin/Plan.tsx
+++ resources/js/pages/Admin/Plan.tsx
@@ -5,20 +5,41 @@ import { toast } from "sonner";
 import { Toaster } from "@/components/ui/sonner"
 import WeeklyPlans from "@/components/admin/WeeklyPlans";
 
-export default function Plan({ weeklyPlans }) {
-    const { flash } = usePage().props
+interface PlanProps {
+    weeklyPlans: WeeklyPlan[],
+}
+
+interface WeeklyPlan {
+    id: number,
+    start_date: string,
+}
+
+interface FlashProps {
+    message?: string,
+}
+
+type UsePageProp = {
+    flash: FlashProps,
+};
+
+// Add types to fix Typescript errors
+export default function Plan({ weeklyPlans }: PlanProps) {
+    const { flash } = usePage<UsePageProp>().props;
     if (flash.message) {
         toast.success(flash.message);
     }
 
     return (
+        // Use layout for all admin pages to reduce repetition
         <AdminLayout>
             <Head title="Weekly Planning" />
             <Toaster position="top-center" richColors />
+            {/* This is the main content container */}
             <div className="space-y-4">
                 <div className="flex justify-between items-center">
                     <h1 className="font-semibold text-xl">Weekly Plans</h1>
                     <Button asChild className="w-fit" variant="outline">
+                        {/* Use route() method to avoid hard-coding routes as strings */}
                         <Link href={route('weekly-plans.store')} method="post">Create Weekly Plan</Link>
                     </Button>
                 </div>
