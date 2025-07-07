b13d235ec3fe7b5ddf64b7ef15eed22b4c008317
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-07-07 11:41:25 +0000

    refactor(admin/users/`Unverified`): use tanstack table

--- resources/js/components/admin/users/Unverified.tsx
+++ resources/js/components/admin/users/Unverified.tsx
@@ -11,6 +11,12 @@ import { Link } from "@inertiajs/react";
 import { TableRow, TableCell, Table, TableBody, TableHead, TableHeader } from "@/components/ui/table";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { useState } from "react";
+import {
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    useReactTable,
+} from "@tanstack/react-table"
 
 interface UnverifiedMember {
     id: number,
@@ -26,65 +32,137 @@ export default function Unverified({ unverifiedMembers }: { unverifiedMembers: U
     const [proofPath, setProofPath] = useState('');
     const [conditionPath, setConditionPath] = useState('');
 
+    const columns = [
+        {
+            accessorKey: 'id',
+            header: 'Member ID',
+        },
+        {
+            accessorKey: 'name',
+            header: 'Name',
+        },
+        {
+            accessorKey: 'birth_date',
+            header: 'Birthdate',
+        },
+        {
+            accessorKey: 'action',
+            header: 'Action',
+            cell: ({ row }) => (
+                <DialogTrigger asChild>
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            setToVerify(row.original.id);
+                            setProofPath(row.original.proof_of_identity);
+                            setConditionPath(row.original.medical_condition);
+                        }}
+                    >
+                        Check
+                    </Button>
+                </DialogTrigger>
+            ),
+        },
+    ];
+
+    const table = useReactTable({
+        data: unverifiedMembers,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+    })
+
     return (
-        <div className="border rounded-xl">
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Member ID</TableHead>
-                        <TableHead>Name</TableHead>
-                        <TableHead>Birthdate</TableHead>
-                        <TableHead>Action</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-                        {unverifiedMembers.map(member => (
-                            <TableRow key={member.id}>
-                                <TableCell className="font-medium">{member.id}</TableCell>
-                                <TableCell>{member.name}</TableCell>
-                                <TableCell>{member.birth_date}</TableCell>
-                                <TableCell className="flex gap-2">
-                                    <DialogTrigger asChild>
-                                        <Button variant="outline" onClick={() => {
-                                            setToVerify(member.id);
-                                            setProofPath(member.proof_of_identity);
-                                            setConditionPath(member.medical_condition);
-                                        }}>Check</Button>
-                                    </DialogTrigger>
-                                </TableCell>
+        <div>
+            <div className="border rounded-xl">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map(header => (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                ))}
                             </TableRow>
                         ))}
-                        <DialogContent>
-                            <DialogHeader>
-                                <DialogTitle>Verify User?</DialogTitle>
-                                <DialogDescription>Please confirm that the submitted files are legit</DialogDescription>
-                                <div>
-                                    <Accordion type="single" collapsible>
-                                        <AccordionItem value="identity">
-                                            <AccordionTrigger>Proof of Identity</AccordionTrigger>
-                                            <AccordionContent>
-                                                <img src={"/api/images/"+proofPath}/>
-                                            </AccordionContent>
-                                        </AccordionItem>
-                                        {conditionPath && (
-                                            <AccordionItem value="condition">
-                                                <AccordionTrigger>Medical Condition</AccordionTrigger>
+                    </TableHeader>
+                    <TableBody>
+                        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+                            {table.getRowModel().rows?.length ? (
+                                table.getRowModel().rows.map(row => (
+                                    <TableRow
+                                        key={row.id}
+                                        data-state={row.getIsSelected() && "selected"}
+                                    >
+                                        {row.getVisibleCells().map(cell => (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))
+                            ) : (
+                                    <TableRow>
+                                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                                            No results.
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Verify User?</DialogTitle>
+                                    <DialogDescription>Please confirm that the submitted files are legit</DialogDescription>
+                                    <div>
+                                        <Accordion type="single" collapsible>
+                                            <AccordionItem value="identity">
+                                                <AccordionTrigger>Proof of Identity</AccordionTrigger>
                                                 <AccordionContent>
-                                                    <img src={"/api/images/"+conditionPath} />
+                                                    <img src={"/api/images/"+proofPath}/>
                                                 </AccordionContent>
                                             </AccordionItem>
-                                        )}
-                                    </Accordion>
-                                </div>
-                                <Button asChild onClick={()=>setDialogOpen(false)}>
-                                    <Link href={route('members.verify', toVerify)} method="patch">Verify</Link>
-                                </Button>
-                            </DialogHeader>
-                        </DialogContent>
-                    </Dialog>
-                </TableBody>
-            </Table>
+                                            {conditionPath && (
+                                                <AccordionItem value="condition">
+                                                    <AccordionTrigger>Medical Condition</AccordionTrigger>
+                                                    <AccordionContent>
+                                                        <img src={"/api/images/"+conditionPath} />
+                                                    </AccordionContent>
+                                                </AccordionItem>
+                                            )}
+                                        </Accordion>
+                                    </div>
+                                    <Button asChild onClick={()=>setDialogOpen(false)}>
+                                        <Link href={route('members.verify', toVerify)} method="patch">Verify</Link>
+                                    </Button>
+                                </DialogHeader>
+                            </DialogContent>
+                        </Dialog>
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+            </div>
         </div>
     );
 }
