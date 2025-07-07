ad9ab5f3072fe868a228b5fab687cadd1d0bdc64
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-07-07 13:57:21 +0000

    feat(`app-logo-icon`): add default value for prop

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,4 +1,4 @@
-export default function AppLogoIcon({ className }) {
+export default function AppLogoIcon({ className = "" }) {
     return (
         <div className={className}>
             <img src="/storage/images/meals-on-wheels-logo.svg" alt="Meals on Wheels logo"/>
