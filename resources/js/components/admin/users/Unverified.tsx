a0d30198134953ea9fb5aaf83fd567650db4e4ee
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-07-07 10:29:41 +0000

    refactor: make separate component for unverified members table

--- resources/js/components/admin/users/Unverified.tsx
+++ resources/js/components/admin/users/Unverified.tsx
@@ -1,46 +1,90 @@
 import { Button } from "@/components/ui/button";
-import { 
+import {
     DialogHeader,
     Dialog,
     DialogTrigger,
     DialogContent,
     DialogTitle,
-    DialogDescription 
- } from "@/components/ui/dialog";
+    DialogDescription
+} from "@/components/ui/dialog";
 import { Link } from "@inertiajs/react";
-import { TableRow, TableCell } from "@/components/ui/table";
+import { TableRow, TableCell, Table, TableBody, TableHead, TableHeader } from "@/components/ui/table";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { useState } from "react";
 
-export default function Unverified({ users }) {
-    const [userId, setUserId] = useState(null);
+interface UnverifiedMember {
+    id: number,
+    name: string,
+    birth_date: string,
+    proof_of_identity: string,
+    medical_condition: string,
+}
+
+export default function Unverified({ unverifiedMembers }: { unverifiedMembers: UnverifiedMember[] }) {
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [toVerify, setToVerify] = useState(0);
+    const [proofPath, setProofPath] = useState('');
+    const [conditionPath, setConditionPath] = useState('');
 
     return (
-        <Dialog>
-            {users.map(user => (
-                <TableRow key={user.id}>
-                    <TableCell className="font-medium">{user.id}</TableCell>
-                    <TableCell>{user.first_name}</TableCell>
-                    <TableCell>{user.last_name}</TableCell>
-                    <TableCell>{user.userable_type}</TableCell>
-                    <TableCell className="flex gap-2">
-                        <DialogTrigger asChild>
-                            <Button variant="outline" onClick={()=>setUserId(user.id)}>Check</Button>
-                        </DialogTrigger>
-                    </TableCell>
-                </TableRow>
-            ))}
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
-                    </DialogDescription>
-                    <Button asChild>
-                        <Link href={route('members.verify')} method="post" data={{ user_id: userId }}>Verify</Link>
-                    </Button>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
+        <div className="border rounded-xl">
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Member ID</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Birthdate</TableHead>
+                        <TableHead>Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+                        {unverifiedMembers.map(member => (
+                            <TableRow key={member.id}>
+                                <TableCell className="font-medium">{member.id}</TableCell>
+                                <TableCell>{member.name}</TableCell>
+                                <TableCell>{member.birth_date}</TableCell>
+                                <TableCell className="flex gap-2">
+                                    <DialogTrigger asChild>
+                                        <Button variant="outline" onClick={() => {
+                                            setToVerify(member.id);
+                                            setProofPath(member.proof_of_identity);
+                                            setConditionPath(member.medical_condition);
+                                        }}>Check</Button>
+                                    </DialogTrigger>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Verify User?</DialogTitle>
+                                <DialogDescription>Please confirm that the submitted files are legit</DialogDescription>
+                                <div>
+                                    <Accordion type="single" collapsible>
+                                        <AccordionItem value="identity">
+                                            <AccordionTrigger>Proof of Identity</AccordionTrigger>
+                                            <AccordionContent>
+                                                <img src={"/api/images/"+proofPath}/>
+                                            </AccordionContent>
+                                        </AccordionItem>
+                                        {conditionPath && (
+                                            <AccordionItem value="condition">
+                                                <AccordionTrigger>Medical Condition</AccordionTrigger>
+                                                <AccordionContent>
+                                                    <img src={"/api/images/"+conditionPath} />
+                                                </AccordionContent>
+                                            </AccordionItem>
+                                        )}
+                                    </Accordion>
+                                </div>
+                                <Button asChild onClick={()=>setDialogOpen(false)}>
+                                    <Link href={route('members.verify', toVerify)} method="patch">Verify</Link>
+                                </Button>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
+                </TableBody>
+            </Table>
+        </div>
     );
 }
--- resources/js/pages/Admin/Users.tsx
+++ resources/js/pages/Admin/Users.tsx
@@ -1,14 +1,10 @@
 import AdminLayout from '@/layouts/AdminLayout';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
-import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { Head, Link, usePage } from '@inertiajs/react';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Head, usePage } from '@inertiajs/react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { toast } from "sonner";
 import { Toaster } from "@/components/ui/sonner";
-import { useState } from 'react';
 import { UsersTable } from '@/components/admin/users/UsersTable';
+import Unverified from '@/components/admin/users/Unverified';
 
 interface UsersProp {
     unverifiedMembers: Unverified[],
@@ -43,10 +39,6 @@ export default function Users({ unverifiedMembers, users }: UsersProp) {
         }, 0)
     }
 
-    const [dialogOpen, setDialogOpen] = useState(false);
-    const [toVerify, setToVerify] = useState(0);
-    const [proofPath, setProofPath] = useState('');
-    const [conditionPath, setConditionPath] = useState('');
 
     return (
         // Use layout template to reduce code repetition
@@ -65,65 +57,7 @@ export default function Users({ unverifiedMembers, users }: UsersProp) {
                         <UsersTable data={users} />
                     </TabsContent>
                     <TabsContent value="unverified">
-                        <div className="border rounded-xl">
-                            <Table>
-                                <TableHeader>
-                                    <TableRow>
-                                        <TableHead>Member ID</TableHead>
-                                        <TableHead>Name</TableHead>
-                                        <TableHead>Birthdate</TableHead>
-                                        <TableHead>Action</TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-                                        {unverifiedMembers.map(member => (
-                                            <TableRow key={member.id}>
-                                                <TableCell className="font-medium">{member.id}</TableCell>
-                                                <TableCell>{member.name}</TableCell>
-                                                <TableCell>{member.birth_date}</TableCell>
-                                                <TableCell className="flex gap-2">
-                                                    <DialogTrigger asChild>
-                                                        <Button variant="outline" onClick={() => {
-                                                            setToVerify(member.id);
-                                                            setProofPath(member.proof_of_identity);
-                                                            setConditionPath(member.medical_condition);
-                                                        }}>Check</Button>
-                                                    </DialogTrigger>
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                        <DialogContent>
-                                            <DialogHeader>
-                                                <DialogTitle>Verify User?</DialogTitle>
-                                                <DialogDescription>Please confirm that the submitted files are legit</DialogDescription>
-                                                <div>
-                                                    <Accordion type="single" collapsible>
-                                                        <AccordionItem value="identity">
-                                                            <AccordionTrigger>Proof of Identity</AccordionTrigger>
-                                                            <AccordionContent>
-                                                                <img src={"/api/images/"+proofPath}/>
-                                                            </AccordionContent>
-                                                        </AccordionItem>
-                                                        {conditionPath && (
-                                                            <AccordionItem value="condition">
-                                                                <AccordionTrigger>Medical Condition</AccordionTrigger>
-                                                                <AccordionContent>
-                                                                    <img src={"/api/images/"+conditionPath} />
-                                                                </AccordionContent>
-                                                            </AccordionItem>
-                                                        )}
-                                                    </Accordion>
-                                                </div>
-                                                <Button asChild onClick={()=>setDialogOpen(false)}>
-                                                    <Link href={route('members.verify', toVerify)} method="patch">Verify</Link>
-                                                </Button>
-                                            </DialogHeader>
-                                        </DialogContent>
-                                    </Dialog>
-                                </TableBody>
-                            </Table>
-                        </div>
+                        <Unverified unverifiedMembers={unverifiedMembers} />
                     </TabsContent>
                 </Tabs>
             </div>
