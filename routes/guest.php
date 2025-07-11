f4075644e0bbbdd31edb0dd10e3a1eb8d4a49ef6
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-07-11 17:41:38 +0000

    Group routes that uses the same controller

--- routes/guest.php
+++ routes/guest.php
@@ -4,13 +4,18 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DonationController;
 use App\Http\Controllers\GuestController;
 
-Route::get('/', [GuestController::class, 'home'])->name('home');
-Route::get('/about', [GuestController::class, 'about'])->name('about');
-Route::get('/contact', [GuestController::class, 'contact'])->name('contact');
-Route::get('/donation', [GuestController::class, 'donation'])->name('donation');
-Route::get('/donation/success', [DonationController::class, 'success'])->name('donation.success');
+Route::controller(GuestController::class)->group(function () {
+    Route::get('/', 'home')->name('home');
+    Route::get('/about', 'about')->name('about');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::get('/donation', 'donation')->name('donation');
+    Route::get('/donation/success', 'success')->name('donation.success');
+});
 
 Route::post('/contact', [ContactController::class, 'store']);
-Route::post('/donations', [DonationController::class, 'store'])->name('donations.store');
-Route::post('/donations/create-payment-intent', [DonationController::class, 'createPaymentIntent'])->name('donations.create-payment-intent');
-Route::post('/stripe/webhook', [DonationController::class, 'handleWebhook'])->name('stripe.webhook');
+
+Route::controller(DonationController::class)->group(function () {
+    Route::post('/donations', 'store')->name('donations.store');
+    Route::post('/donations/create-payment-intent', 'createPaymentIntent')->name('donations.create-payment-intent');
+    Route::post('/stripe/webhook', 'handleWebhook')->name('stripe.webhook');
+});
